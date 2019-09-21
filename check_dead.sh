86bba2f33b52e883f7d7f969af2221744c23f7f8
tevko <tevko@1819health.com>
2019-09-21 01:38:23 -0400

    adjust dead url checker

--- check_dead.sh
+++ check_dead.sh
@@ -5,8 +5,10 @@ for file in *.md; do
 		url=$(echo "$line" | sed s/.*\(// | sed s/\)// | sed s/^##.*//)
 
 		if [[ ! -z "$url" ]]; then
-			echo "- Checking URL: ${url}"
-			curl -sI "${url}" | grep "HTTP/1.[01] [^23].."
+			status_code=$(curl -m 10 -s -o /dev/null -w "%{http_code}" $url)
+			if [ $status_code == "000" ] || [ $status_code == "404" ] || [ $status_code == "403" ] || [ $status_code == "400" ]; then
+				echo "${line} ${status_code}"
+			fi
 		fi
 	done <"$file"
 done
