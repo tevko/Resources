c05af800325af1811c7b59253680dbde1021556e
tevko <timevko@gmail.com>
2021-11-06 16:10:40 -0500

    begin promise

--- js/main.js
+++ js/main.js
@@ -1,3 +1,14 @@
 import files from '../db/dictionary.js';
 
-console.log(files);
+const loading = true;
+let search_struct;
+
+const promises = files.map(file => fetch(file));
+
+await Promise.all(promises)
+  .then(responses => Promise.all(responses.map(res => res.text())))
+  .then(texts => search_struct = texts.join('/n'));
+
+loading = false;
+
+console.log(search_struct, loading);
