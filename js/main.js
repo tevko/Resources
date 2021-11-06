25c03fc193a5d4a2d7257455d4d75db96c6874ff
tevko <timevko@gmail.com>
2021-11-06 16:28:51 -0500

    regex and better search struct

--- js/main.js
+++ js/main.js
@@ -1,13 +1,17 @@
 import files from '../db/dictionary.js';
 
-const loading = true;
-let search_struct;
+let loading = true;
+const search_struct = {};
 
 const promises = files.map(file => fetch(file));
 
 await Promise.all(promises)
   .then(responses => Promise.all(responses.map(res => res.text())))
-  .then(texts => search_struct = texts.join('/n'));
+  .then(texts => texts.forEach(t => {
+    const reg = /(?:##) (.+)/g;
+    const title = reg.exec(t)[1];
+    search_struct[title] = t;
+  }));
 
 loading = false;
 
